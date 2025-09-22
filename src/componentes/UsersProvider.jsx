7984637d87a16db4c8dc6271453ed866527c6048
uaggit99 <ace77ey@gmail.com>
2025-09-22 16:10:15 -0500

    uso de local stotage

--- src/componentes/UsersProvider.jsx
+++ src/componentes/UsersProvider.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import dataJson from '../assert/usuario.json'
 import { UserContext } from '../contexts/UserContext'
+//import { useLocalStorage } from '../hooks/useLocalStorage';
 
 
 
 
 export const  UsersProvider=({children})=>{
 
-    const[users, setUsers]=useState(dataJson);
+    const[users, setUsers]=useState( dataJson);
     return(
         <UserContext.Provider value={{users, setUsers}}>
             {children}
